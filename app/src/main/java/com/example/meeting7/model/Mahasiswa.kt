47908b0f4b405a2e6e7ad7df7537bd9cbac42ee6
adandoang <akhdanjzif@gmail.com>
2024-11-16 22:24:55 +0700

    menambahkan variabel email, no hp, dan nim pada class Mahasiswa

--- app/src/main/java/com/example/meeting7/model/Mahasiswa.kt
+++ app/src/main/java/com/example/meeting7/model/Mahasiswa.kt
@@ -3,5 +3,8 @@ package com.example.meeting7.model
 data class Mahasiswa(
     val nama : String ="",
     val gender : String ="",
-    val alamat : String = ""
+    val alamat : String = "",
+    val email : String = "",
+    val nohp : String = "",
+    val nim : String = ""
 )
